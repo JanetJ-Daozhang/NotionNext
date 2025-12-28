056a7fe86d3f56e08348496c767b4f0d28c3bd9e
JanetJ <47096159+JanetJ-Daozhang@users.noreply.github.com>
2025-12-28 14:37:28 +0800

    Add comma to FONT_SANS array in font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -22,7 +22,7 @@ module.exports = {
   FONT_SUBSET: process.env.NEXT_PUBLIC_FONT_SUBSET || 'chinese-simplified',
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
-    '"MaoKen TangYuan"'
+    '"MaoKen TangYuan"',
     // '"LXGW WenKai"',
     '"PingFang SC"',
     '-apple-system',
